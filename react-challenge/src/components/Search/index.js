56125cecc094b65f00d0d6f1a61db0fb69197e3a
Emmanuel Egomson <fegomson@gmail.com>
2021-04-26 18:03:55 -0500

    Implemented the api-call in the Search component

--- react-challenge/src/components/Search/index.js
+++ react-challenge/src/components/Search/index.js
@@ -1,25 +1,34 @@
 import React from 'react'
+import axios from 'axios'
 
 const Search = () => {
   return (
     <>
-      <form>
-        <div className="form-group">
-          <input 
-            type="text" 
-            placeholder="Search giphs..." 
-            className="form-control"
-            onChange={handleFormChange}
-          />
-        </div>
-      </form>
+      <div className="form-group">
+        <input 
+          type="text" 
+          placeholder="Search giphs..." 
+          className="form-control"
+          onChange={searchTextChange}
+        />
+      </div>
     </>
   )
 }
 
-const handleFormChange = (e) => {
-  e.preventDefault()
-  console.log(e.target.value)
+const searchTextChange = (e) => {
+  const apiUrl = 'http://api.giphy.com/v1/gifs/search'
+  const api_key = '0s8YhD2rpCePplp27WvsmYEPqV1wYNE0'
+  const queryString = e.target.value
+  const limit = 20
+  axios(`${apiUrl}`, {
+    params: {
+      api_key,
+      q: queryString,
+      limit
+    }
+  })
+  .then(res => console.log(res.data.data))
 }
 
 export default Search
