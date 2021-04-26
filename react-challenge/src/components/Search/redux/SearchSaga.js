f8d8fb2fc5f7b56fb3bdfaa6f20c6146b9034562
Emmanuel Egomson <fegomson@gmail.com>
2021-04-26 18:32:33 -0500

    SearchSage setSearch and watchSearch setup

---
+++ react-challenge/src/components/Search/redux/SearchSaga.js
@@ -0,0 +1,28 @@
+import { call, put, takeLatest } from "redux-saga/effects"
+import types from "../types"
+
+
+function* setSearch({ value }) {
+  try {
+    const apiUrl = 'http://api.giphy.com/v1/gifs/search'
+    const api_key = '0s8YhD2rpCePplp27WvsmYEPqV1wYNE0'
+    const queryString = e.target.value
+    const limit = 20
+    const result = yield axios(`${apiUrl}`, {
+      params: {
+        api_key,
+        q: queryString,
+        limit
+      }
+    })
+    console.log(result.data.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function* watchSearch() {
+  yield takeLatest(types.watchGetSearchResults, setSearch)
+}
+
+export default watchSearch
--- react-challenge/src/components/Search/types.js
+++ react-challenge/src/components/Search/types.js
@@ -1,3 +1,4 @@
 export default {
-  addSearchResult: "ADD_SEARCH_RESULT"
+  addSearchResult: "ADD_SEARCH_RESULT",
+  watchGetSearchResults: "WATCH_GET_SEARCH_RESULTS"
 }
